ed2f50fce243872d1c3fb48f88e21586b84d4b7f
IonutBotizan <botizanionut1986@gmail.com>
2017-06-19 17:43:06 +0300

    Delete AdvertiseSearchViewModel.cs

--- AdvertiseSearchViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SellYourCar.ViewModels
-{
-    public class AdvertiseSearchViewModel
-    {
-        public decimal? PriceFrom {get; set; }
-        public decimal? PriceTo {get; set; }
-    }
-}
