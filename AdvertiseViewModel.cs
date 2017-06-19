9980388ffbd511899298e575faedda7161383541
IonutBotizan <botizanionut1986@gmail.com>
2017-06-19 17:17:18 +0300

    Add files via upload

---
+++ AdvertiseViewModel.cs
@@ -0,0 +1,28 @@
+using System;
+using SellYourCar.Entities;
+
+namespace SellYourCar.ViewModels
+{
+    public class AdvertiseViewModel
+    {
+
+        public string Url {get; set; }
+        public string FullName {get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public string PhotoUrl { get; set; }
+        public decimal Mileage { get; set; }
+        public int Power { get; set; }
+
+        public int Year { get; set; }
+        public string UserWhoAdded { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        //lookups
+        public string Fuel { get; set; }
+        public string[] Fuels {get; set; }
+
+    }
+}
