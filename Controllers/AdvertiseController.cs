065d4e6c1e17c752efdd4bda8a0db6327143d1eb
IonutBotizan <botizanionut1986@gmail.com>
2017-06-19 18:54:11 +0300

    Todo Change Pagination  Use Header for TotalItems

--- Controllers/AdvertiseController.cs
+++ Controllers/AdvertiseController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using SellYourCar.Entities;
 using SellYourCar.Repos;
 using SellYourCar.ViewModels;
 
